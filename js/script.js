97ec930491a79efa66b5989570da146480a8c146
Boort <Boort>
2016-08-29 16:44:43 +0300

    add script

---
+++ js/script.js
@@ -0,0 +1,30 @@
+(function(){
+  if(document.querySelector(".slider")){
+    var sliderItems = document.querySelectorAll(".slider-item");
+    var buttons = document.querySelectorAll("input[name='slider']");
+    buttons[0].checked = "true";
+    for(var k = 0; k < buttons.length; k++){
+      buttons[k].addEventListener('click', function(e){
+        var data = e.target.dataset;
+        for(var i = 0; i < sliderItems.length; i++){
+          sliderItems[i].style.display = "none";
+        }
+        sliderItems[data.count].style.display = "block";
+      })
+    }
+  }
+})();
+
+!function(){
+  var writeUsBtn = document.querySelector("#write-us");
+  var popup = document.querySelector(".popup");
+  var closeBtn = document.querySelector(".popup__btn-close");
+  writeUsBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    popup.classList.add("block-show");
+  });
+  closeBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    popup.classList.remove("block-show");
+  })
+}();
